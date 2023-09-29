815212c586caf3b174fcb71147c8bb987d835a08
Adonothing <134787584+Adonothing@users.noreply.github.com>
2023-09-29 13:32:27 +0800

    2023年9月29日

---
+++ VS/Solution/VS2022/first/first.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	//输入商品数量
+	int N;
+	cin >> N;
+
+	//输入商品价格
+	int p[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> p[i];
+	}
+
+	//计算每个商品优惠价格
+	int money[N] = { 0 };
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i - 1; j >= 0;j--)
+		{
+			if (p[j] <= p[i])
+			{
+				money[i] = p[j];
+				break;
+			}
+		}
+	}
+
+	//计算总商品优惠价格
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		sum += money[i];
+	}
+	cout << sum;
+
+	return 0;
+}
